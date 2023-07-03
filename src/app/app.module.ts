859ec1883739e2580d5603c8af0f17e5e3ed0072
Jhonatas Gomes Ribeiro <125414092+jhonatasjgr@users.noreply.github.com>
2023-07-03 11:20:44 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DialogoComponent } from './dialogo/dialogo.component';
+import { FirstComponent } from './first/first.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogoComponent,
+    FirstComponent
   ],
   imports: [
     BrowserModule,
